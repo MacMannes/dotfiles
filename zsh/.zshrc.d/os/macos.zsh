671428499fcf6a8a82b2764b0492a3af541b3049
André Mathlener <andre.mathlener@ns.nl>
2025-12-21 15:55:21 +0100

    fix: make zsh work again on macOS

--- zsh/.zshrc.d/os/macos.zsh
+++ zsh/.zshrc.d/os/macos.zsh
@@ -1,3 +1,15 @@
+# Cache Homebrew prefix across shells
+if [[ -f ~/.cache/brew_prefix ]]; then
+  export HOMEBREW_PREFIX="$(< ~/.cache/brew_prefix)"
+else
+  mkdir -p ~/.cache
+  HOMEBREW_PREFIX="$(brew --prefix)"
+  echo "$HOMEBREW_PREFIX" > ~/.cache/brew_prefix
+  export HOMEBREW_PREFIX
+fi
+
+PATH=${HOMEBREW_PREFIX}/bin/:$PATH
+
 # Antidote via Homebrew
 if [[ -n "$HOMEBREW_PREFIX" ]] &&
    [[ -f "$HOMEBREW_PREFIX/opt/antidote/share/antidote/antidote.zsh" ]]; then
