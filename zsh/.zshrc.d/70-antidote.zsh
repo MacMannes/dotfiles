698381c08b6ee30156affca6da0220d70c7f5d60
Andre Mathlener <info@macmannes.nl>
2025-12-21 14:57:53 +0100

    fix: remove debug echo commands

--- zsh/.zshrc.d/70-antidote.zsh
+++ zsh/.zshrc.d/70-antidote.zsh
@@ -1,5 +1,4 @@
 if command -v antidote >/dev/null; then
-  echo loading antidote
   antidote load
 fi
 
--- zsh/.zshrc.d/os/linux.zsh
+++ zsh/.zshrc.d/os/linux.zsh
@@ -1,5 +1,4 @@
 # Antidote via system packages
-echo "sourcing antidote"
 if [[ -f /usr/share/zsh-antidote/antidote.zsh ]]; then
   source /usr/share/zsh-antidote/antidote.zsh
 fi
