f0c3c1a2f3b8e25b769093aa564d303cdfdbb0e2
Andre Mathlener <info@macmannes.nl>
2025-12-22 22:49:25 +0100

    fix(nvim): call snacks.bufdelete in stead of snacks.bufDelete

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -7,29 +7,32 @@ return {
     },
     keys = {
         -- stylua: ignore start
-        { "<leader><leader>", function() Snacks.picker.buffers() end, desc = "Find existing buffers" },
+        { "<leader><leader>", function() Snacks.picker.buffers() end,                desc = "Find existing buffers" },
+        { "<D-e>",            function() Snacks.explorer() end,                      desc = "File Explorer", },
+        { "<D-o>",            function() Snacks.picker.files({ hidden = true }) end, desc = "Find Files" },
+        { "<D-w>",            function() Snacks.bufdelete() end,                     desc = "Delete Buffer" },
+        { "z=",               function() snacks.picker.spelling() end,               desc = "spelling suggestions" },
 
-        { "<D-e>", function() Snacks.explorer() end, desc = "File Explorer", },
-        { "<D-o>", function() Snacks.picker.files({ hidden = true}) end, desc = "Find Files" },
-        { "<D-w>", function() Snacks.bufDelete() end, desc = "Delete Buffer" },
-        { "z=", function() snacks.picker.spelling() end, desc = "spelling suggestions" },
 
+        {
+            "<leader>fp",
+            function()
+                Snacks.picker.projects(
+                    {
+                        dev = { "~/Develop/NS/Backend/",
+                            "~/Develop/NS/Cloud/",
+                            "~/Develop/NS/Dojo/",
+                            "~/Develop/NS/Serverless/",
+                            "~/Develop/NS/Web/",
+                            "~/Develop/MacMannes/Backend/",
+                            "~/Develop/MacMannes/Cloud/",
+                            "~/Develop/MacMannes/Embedded/",
+                            "~/Develop/MacMannes/Serverless/",
+                            "~/Develop/MacMannes/HomeAutomation/",
 
-        { "<leader>fp", function() Snacks.picker.projects(
-            { dev = { "~/Develop/NS/Backend/",
-                      "~/Develop/NS/Cloud/",
-                      "~/Develop/NS/Dojo/",
-                      "~/Develop/NS/Serverless/",
-                      "~/Develop/NS/Web/",
-                      "~/Develop/MacMannes/Backend/",
-                      "~/Develop/MacMannes/Cloud/",
-                      "~/Develop/MacMannes/Embedded/",
-                      "~/Develop/MacMannes/Serverless/",
-                      "~/Develop/MacMannes/HomeAutomation/",
-
-                    },
-                    patterns = { ".git", "package.json", "gradlew" },
-                })
+                        },
+                        patterns = { ".git", "package.json", "gradlew" },
+                    })
             end,
             desc = "Projects",
         },
