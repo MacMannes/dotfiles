103cc23c9190208a5cb628e086b1a9537850b25f
André Mathlener <andre.mathlener@ns.nl>
2026-02-20 11:32:31 +0100

    refactor: cleanup zshrc

--- zsh/.zshrc.d/20-completion.zsh
+++ zsh/.zshrc.d/20-completion.zsh
@@ -4,3 +4,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' menu no
 zstyle ':fzf-tab:*' fzf-flags --color=fg:4
 
+# bun completions
+[ -s "/Users/andre/.bun/_bun" ] && source "/Users/andre/.bun/_bun"
+
